ae93fb5e132d86fcb717d57d39f7a123c1faa539
QuixoticTendencies <ira.domine@gmail.com>
2014-11-09 22:29:07 -0800

    Update button.lua

--- qHaul/button.lua
+++ qHaul/button.lua
@@ -1,5 +1,10 @@
 addonname, namespace = ...
 
-local button = addonname .. "_Button" 
-CreateFrame("BUTTON", button, "MerchantFrame")
+local button = CreateFrame("BUTTON", nil, MerchantFrame)
 button:RegisterForClicks("LeftButton", "RightButton")
+button:SetHeight(32)
+button:SetWidth(32)
+local texture = button:CreateTexture()
+texture:SetTexture("Interface\\Icons\\inv_misc_bag_10_black")
+texture:SetAllPoints(button)
+button:SetPoint("TOPLEFT", -32, 0)
