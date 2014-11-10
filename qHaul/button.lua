291d6d6f722b089521da2776680e0c76d7abeb69
QuixoticTendencies <ira.domine@gmail.com>
2014-11-09 22:41:57 -0800

    Update button.lua

--- qHaul/button.lua
+++ qHaul/button.lua
@@ -1,10 +1,15 @@
 addonname, namespace = ...
 
-local button = CreateFrame("BUTTON", nil, MerchantFrame)
-button:RegisterForClicks("LeftButton", "RightButton")
-button:SetHeight(32)
-button:SetWidth(32)
-local texture = button:CreateTexture()
+local btn = CreateFrame("BUTTON", nil, MerchantFrame)
+btn:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+btn:SetHeight(32)
+btn:SetWidth(32)
+local texture = btn:CreateTexture()
 texture:SetTexture("Interface\\Icons\\inv_misc_bag_10_black")
-texture:SetAllPoints(button)
-button:SetPoint("TOPLEFT", -32, 0)
+texture:SetAllPoints(btn)
+btn:SetPoint("TOPLEFT", -32, 0)
+btn:SetScript("OnClick", btn.OnClick)
+
+local function btn.OnClick(self, button)
+  print("You have clicked on " .. addonname .. "'s button with the " .. button)
+end
